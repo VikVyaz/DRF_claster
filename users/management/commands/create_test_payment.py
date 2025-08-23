49cbd1ac653d39d90160673089a5a0abe0b7383d
VikVyaz <gluckundhappy@gmail.com>
2025-08-24 00:31:10 +0300

    Спорное решение, но вроде норм

--- users/management/commands/create_test_payment.py
+++ users/management/commands/create_test_payment.py
@@ -19,17 +19,41 @@ class Command(BaseCommand):
             user.save()
             self.stdout.write(self.style.SUCCESS(f"Создан пользователь {user.email}"))
 
-        course, _ = Course.objects.get_or_create(name="Тестовый курс")
-        self.stdout.write(self.style.SUCCESS(f"Создан курс: {course.name}"))
+        # course, _ = Course.objects.get_or_create(name="Тестовый курс")
+        # self.stdout.write(self.style.SUCCESS(f"Создан курс: {course.name}"))
 
-        lesson, _ = Lesson.objects.get_or_create(
-            name="Тестовый урок",
-            course=course,
-            defaults={
-                "lesson_link": "https://test.com"
-            }
-        )
-        self.stdout.write(self.style.SUCCESS(f"Создан урок: {lesson.name}"))
+        success = False
+        name_num = 1
+        while success:
+            try:
+                course, _ = Course.objects.get_or_create(name=f"Тестовый курс_{name_num}")
+                self.stdout.write(self.style.SUCCESS(f"Создан курс: {course.name}"))
+                success = True
+            except Exception:
+                name_num += 1
+
+        # lesson, _ = Lesson.objects.get_or_create(
+        #     name="Тестовый урок",
+        #     course=course,
+        #     defaults={
+        #         "lesson_link": "https://test.com"
+        #     }
+        # )
+        # self.stdout.write(self.style.SUCCESS(f"Создан урок: {lesson.name}"))
+
+        success = False
+        name_num = 1
+        while success:
+            try:
+                lesson, _ = Lesson.objects.create(
+                    name=f"Тестовый урок_{name_num}",
+                    course=course,
+                    lesson_link="https://test.com"
+                )
+                self.stdout.write(self.style.SUCCESS(f"Создан урок: {lesson.name}"))
+                success = True
+            except Exception:
+                name_num += 1
 
         payment_course, _ = Payment.objects.get_or_create(
             user=user,
